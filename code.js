d1439fd036136ee22cd91cced9d89da7f87dbf0c
Iftekhar <iftekharshovonseu17@gmail.com>
2022-08-10 01:59:04 +0600

    problem solving

---
+++ code.js
@@ -0,0 +1,90 @@
+
+
+// var i = 5;
+// var j = 10;
+// var l=7;
+// var k = i+j
+//  l = 20; 
+// var korimUddin =["2.445566884", "3.445566884", "4.445566884", "5.445566884"];
+
+// var index = korimUddin.unshift("no");
+// if(index !== -1){
+//   PushManager();
+
+// }
+// var news = korimUddin.indexOf(6);
+//  var korim =parseFloat(korimUddin);
+//  var fixedKorim =korim.toFixed(4);
+//  var numKorim =parseFloat(fixedKorim);
+// console.log(korimUddin);
+// console.log(index);
+// console.log(news);
+// for (i; i < 5; i++) {
+//   console.log(i);
+// }
+// var korim = [200, 150, 100, 50, 250, 40];
+ 
+// for(var i = 50; i<=80; i++){
+//   if(i%2!==0){
+//     console.log(i);
+//   }
+  
+// }
+// function Farehnite(num1 ){
+//   return  (num1*1.8)+32;
+// }
+// function Celcious(num1 ){
+//   return  (num1-32)*.5556;
+// }
+// console.log(Farehnite(0));
+// console.log(Celcious(32));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Grade(){
+//   let number = 100;
+  
+//   switch (true) {
+//     case (number<= 0 && number <= 32): console.log('F');
+//        break;
+//     case (number >= 33 && number <= 39): console.log('D');
+//        break;
+//     case (number >= 40 && number <= 49): console.log('C');
+//        break;
+//     case (number >= 50 && number <= 59): console.log('B');
+//        break;
+//     case (number >= 60 && number <= 69): console.log('A-');
+//        break;
+//     case (number >= 70 && number <= 79): console.log('A');
+//        break;
+//     case (number >= 80 && number <= 100): console.log('A+');
+//        break;
+  
+//     default: console.log("input a number to see your grade");
+//   }
+//   return number;
+// }
+// console.log(Grade());
+
+
+  // let i = 0;
+
+  // while (i < 6) {
+  //   if (i === 3) {
+  //     break;
+  //   }
+  //   i ++;
+  //   console.log(i);
+  // }
+
+
